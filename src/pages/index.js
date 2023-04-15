f3da80567fea7e7b7b410812361e8d365f3e9ff8
Jaydeep Patel <mcpe071202@gmail.com>
2023-04-15 13:22:02 +0530

    added picture generator code

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,42 @@ export default function Home() {
         <div className={styles.mainContent}>
           <div className={styles.generatorLeft}></div>
           <div className={styles.generatorRight}>
-            <div className={styles.generator}></div>
+            <div className={styles.generator}>
+              <div className="sketch" style={{ width: "100%", height: "100%" }}>
+                <iframe
+                  className="p5frame"
+                  style={{ width: "100%", height: "100%" }}
+                  srcDoc={`
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/addons/p5.sound.min.js"></script>
+                <style>
+                    html, body {
+                        margin: 0;
+                        padding: 0;
+                        height: 100vh;
+                        width: 100vw;
+                        display:flex;
+                        flex-direction: column;
+                        justify-content: center;
+                        align-items: center;
+                    }
+                    canvas {
+                        display: block;
+                    }
+                </style>                      
+                <meta charset="utf-8" />
+            
+                </head>
+                <body>
+                <script src=${"/sketch.js"}></script>
+                </body>
+            </html> `}
+                ></iframe>
+              </div>
+            </div>
             <div className={styles.generatorOptions}>
               <button className={styles.discardButton}>Discard</button>
               <button className={styles.storeButton}>store</button>
